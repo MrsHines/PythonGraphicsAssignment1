7038472d356ffd7da4bd5124eeb409e8387d6730
MrsHines <34042849+MrsHines@users.noreply.github.com>
2018-08-27 09:47:45 -0500

    Starter Code

---
+++ pictures.py
@@ -0,0 +1,39 @@
+#Python Graphics Program
+
+# put your name here
+# put a short description of your program here
+
+# YOU MAY ADD ADDITIONAL DEFINITIONS IF YOU NEED THEM FOR ORGANIZATION
+
+#import anything you need here
+
+def main():
+  #call the methods needed to draw the graphics
+  
+  
+# What is the purpose of this definition?
+def archery():
+  # write the archery code here
+    
+    
+
+# What is the purpose of this definition?
+def face():
+  # write the face code here
+    
+# What is the purpose of this definition?
+def winter():
+  # write the winter scene code . here
+
+
+# What is the purpose of this definition?
+def dice():
+  # write the dice code here
+  
+  
+#call main() here.
+  
+    
+
+
+
